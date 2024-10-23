0d8a98848fbff3b0346f6591ae7a73c2a664a6e7
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-23 17:16:30 +0300

    writing data with actions

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,3 @@
-// Test ID: IIDSAT
-
 import { useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
@@ -10,7 +8,6 @@ import {
 
 function Order() {
   const order = useLoaderData();
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
     status,
@@ -52,7 +49,7 @@ function Order() {
 }
 
 export async function loader({ params }) {
-  const order = await getOrder(params.order.orderId);
+  const order = await getOrder(params.orderId);
   return order;
 }
 
