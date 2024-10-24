096b0040c7e71bab806482a261a4d8fe4be40ad6
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-24 08:48:59 +0300

    implement responsive display design

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Yordan</div>;
+  return <div className="hidden text-sm font-semibold">Yordan</div>;
 }
 
 export default Username;
