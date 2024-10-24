d0a54356f78b85b656d2228352ea798e4084373c
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-24 09:44:27 +0300

    implement style on button for order

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Yordan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Yordan</div>;
 }
 
 export default Username;
