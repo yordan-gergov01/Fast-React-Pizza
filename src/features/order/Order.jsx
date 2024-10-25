b05fcaed18a71eb741b568e95a4690f7583e7137
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-25 18:08:55 +0300

    updating data without navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -85,6 +86,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
